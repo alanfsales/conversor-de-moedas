f5a362d016826777e8c146b972546e9acae192a1
AlanFSales <alanfsales27@gmail.com>
2024-04-23 23:45:40 -0300

    Menu, Cosulta a API

--- conversor-de-moedas/src/principal/Principal.java
+++ conversor-de-moedas/src/principal/Principal.java
@@ -1,8 +1,22 @@
 package principal;
 
+import com.google.gson.*;
+import consulta.ConsultaExchangerateAPI;
+import menu.MenuPrincipal;
+import modelo.TabelaMoedas;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Principal {
 
     public static void main(String[] args) {
-        
+
+        MenuPrincipal.opcoes();
+        ConsultaExchangerateAPI.consulta("usd");
+
     }
 }
